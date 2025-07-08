422b1815bbc76916a3095bd350091e3239ea5190
Moonx02 <ryan2020gary@gmail.com>
2025-07-08 20:18:34 -0300

    add pub use operators

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 //Commit test in count mooncocacola
 
 pub mod tensor;
+pub use tensor::operators;
 pub use tensor::tensor::Tensor;
 pub use tensor::tensor::Linear;
 pub use tensor::tensor::Optimizer;
